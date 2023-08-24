4a0da3b8e0f0e46b1e98ae2dfe2fdbff66ae1c9f
Emre Döner <64814220+mrdnr@users.noreply.github.com>
2023-08-24 15:33:50 +0300

    lifestyle.js changed

--- src/lifestyle-page/lfiestyle.js
+++ src/lifestyle-page/lfiestyle.js
@@ -1,22 +1,4 @@
 const mainElement = document.getElementById('lifestyle-page');
-const freshOfTheLineContainer = document.getElementById(
-  'freshOffTheLine-container'
-);
-const bestSellersContainer = document.getElementById('bestSellers-container');
-const razerExclusivesContainer = document.getElementById(
-  'razerExclusives-container'
-);
-const razerLastChanceContainer = document.getElementById(
-  'razerLastChance-container'
-);
-const campainCarouselBannerContainer = document.getElementById(
-  'campainCarouselBanner-container'
-);
-const exclusiveBannercontainer = document.getElementById(
-  'exclusiveBanner-container'
-);
-
-const otherLinksContainer = document.getElementById('otherLinks');
 
 renderHOCs('navbar', 'navbarCss');
 renderHOCs('footer', 'footerCss');
